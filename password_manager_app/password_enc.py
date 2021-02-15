6d0f9abf0be07657f1523acf849f84999b643f34
Artur <artur.strz@wp.pl>
2021-02-15 23:13:30 +0100

    improvement pass decrypt

--- password_manager_app/password_enc.py
+++ password_manager_app/password_enc.py
@@ -31,8 +31,11 @@ def decrypt_password(request, pass_enc):
     )
     key = base64.urlsafe_b64encode(kdf.derive(str.encode(config('KEY2_PASS'))))
     f = Fernet(key)
-    if isinstance(pass_enc, str):
-        pass_enc = str.encode(pass_enc)
 
-    password = f.decrypt(pass_enc)
+    if isinstance(pass_enc, str):
+        pass_bytes = str.encode(pass_enc)
+    else:
+        pass_bytes = pass_enc
+
+    password = f.decrypt(pass_bytes)
     return password.decode()
